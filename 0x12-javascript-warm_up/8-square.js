09fba546622a6e57cd6e2e1e75d02e369d35ea99
ibrahim kehinde <91750163+kenny17a@users.noreply.github.com>
2022-09-05 22:01:35 +0100

    Updated square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-if (isNaN(parseInt(process.argv[2]))) {
-  console.log('Missing size');
-} else {
-  const size = parseInt(process.argv[2]);
-  for (let i = 0; i < size; i++) {
-    let square = ' ';
-    for (let j = 0; j < size; j++) {
-      square += 'X';
-    }
-    console.log(square);
-  }
-}
+
+const { argv } = require('process');
+const size = parseInt(argv[2]);
+
+const printSquare = (size) => {
+  const row = 'X'.repeat(size);
+  for (let i = 0; i < size; i++) console.log(row);
+};
+
+Number.isInteger(size) ? printSquare(size) : console.log('Missing size');
